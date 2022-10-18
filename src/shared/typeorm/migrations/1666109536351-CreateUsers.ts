ac23174f8b79a1f2163daf943152628e20ae3e14
Fabricio-Guima <fsgkof@gmail.com>
2022-10-18 13:23:26 -0300

    migration e model de user

---
+++ src/shared/typeorm/migrations/1666109536351-CreateUsers.ts
@@ -0,0 +1,42 @@
+import { Column, MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateUsers1666109536351 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "users",
+        columns: [
+          {
+            name: "id",
+            type: "uuid",
+            isPrimary: true,
+            generationStrategy: "uuid",
+            default: "uuid_generate_v4()",
+          },
+          {
+            name: "name",
+            type: "varchar",
+          },
+          {
+            name: "email",
+            type: "varchar",
+            isUnique: true,
+          },
+          {
+            name: "password",
+            type: "varchar",
+          },
+          {
+            name: "avatar",
+            type: "varchar",
+            isNullable: true,
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("users");
+  }
+}
