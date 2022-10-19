3f06c61495af8b5f2354160dc2c1fa5d4a3d0190
Fabricio-Guima <fsgkof@gmail.com>
2022-10-19 13:21:21 -0300

    upload de arquivo (img)

---
+++ src/modules/users/services/UpdateUserAvatarService.ts
@@ -0,0 +1,40 @@
+import path from "path";
+import AppError from "@shared/errors/AppError";
+import { hash } from "bcryptjs";
+import { getCustomRepository } from "typeorm";
+import User from "../typeorm/entities/User";
+import UserRepository from "../typeorm/repositories/UsersRepository";
+import uploadConfig from "@config/upload";
+import fs from "fs";
+
+interface IRequest {
+  user_id: string;
+  avatarFilename: string;
+}
+class UpdateUserAvatarService {
+  public async execute({ user_id, avatarFilename }: IRequest): Promise<User> {
+    const userRepository = getCustomRepository(UserRepository);
+
+    const user = await userRepository.findById(user_id);
+
+    if (!user) {
+      throw new AppError("User not found.");
+    }
+
+    if (user.avatar) {
+      const userAvatarFilePath = path.join(uploadConfig.directory, user.avatar);
+      const userAvatarFileExists = await fs.promises.stat(userAvatarFilePath);
+
+      if (userAvatarFileExists) {
+        await fs.promises.unlink(userAvatarFilePath);
+      }
+
+      user.avatar = avatarFilename;
+
+      await userRepository.save(user);
+    }
+    return user;
+  }
+}
+
+export default UpdateUserAvatarService;
